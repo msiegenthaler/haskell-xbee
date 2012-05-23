785db96e7d2c01a80c1f3c6b76d0dfffa3b0d153
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-05-23 22:49:58 +0200

    begun implementing command and add tests for it

---
+++ System/Hardware/XBee/Command.hs
@@ -0,0 +1,48 @@
+module System.Hardware.XBee.Command (
+    -- * FrameId
+    FrameId,
+    frameId,
+    nextFrame,
+    -- * Command
+    CommandId,
+    ModemStatus(..),
+    Commands(..)
+) where
+
+import Data.Word
+import Data.Serialize
+import Control.Monad
+
+
+newtype FrameId = FrameId Word8 deriving (Show, Eq)
+-- | Initial FrameId.
+frameId = FrameId 0
+-- | The next FrameId. The ids are looped (after FrameId 255 follows FrameId 0)
+nextFrame (FrameId i) = FrameId (i+1) --overflow
+
+
+data ModemStatus = HardwareReset
+                 | WatchdogTimerReset
+                 | Associated
+                 | Disassociated
+                 | SyncLost
+                 | CoordinatorRealignment
+                 | CoordinatorStarted deriving (Enum, Show, Eq, Bounded)
+
+newtype CommandId = CommandId (Word8, Word8)
+
+-- | Commands to/from the XBee
+data Commands = ModemStatusUpdate ModemStatus 
+              | ATCommand FrameId CommandId [Word8]
+              | ATQueueCommand FrameId CommandId
+              -- TODO
+
+instance Serialize FrameId where
+    get = liftM FrameId getWord8
+    put (FrameId i) = putWord8 i
+instance Serialize CommandId where
+    get = liftM CommandId $ liftM2 (,) getWord8 getWord8
+    put (CommandId (b1,b2)) = putWord8 b1 >> putWord8 b2
+instance Serialize ModemStatus where
+    get = liftM (toEnum . fromIntegral) getWord8
+    put = putWord8 . fromIntegral . fromEnum
---
+++ test/CommandTest.hs
@@ -0,0 +1,68 @@
+module Main (main) where
+
+import Test.QuickCheck
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Data.Word
+import Data.Serialize
+import System.Hardware.XBee.Command
+import qualified Data.ByteString as BS
+import Control.Monad
+
+
+-- FrameId
+
+frameIdLoopsAroundAfter255 = nextFrame (frameForId 255) == frameId
+
+frameIdSerializeParse :: Word8 -> Bool
+frameIdSerializeParse v = (runGet get $ runPut $ put frame) == Right frame
+    where frame = frameForId v
+
+frameIdParseWord8 w = runGet get (BS.singleton w) == Right (frameForId w)
+
+frameForId :: (Num n, Eq n) => n -> FrameId
+frameForId 0 = frameId
+frameForId i = nextFrame $ frameForId $ i - 1
+
+idForFrame :: FrameId -> Word8
+idForFrame = read . drop 1 . dropWhile (/=' ') . show
+
+instance Arbitrary FrameId where
+    arbitrary = liftM frameForId (choose (0, 255)::Gen Word8)
+
+
+--Modem status
+
+modemStatusSerialize =
+        ser HardwareReset == [0]
+     && ser WatchdogTimerReset == [1]
+     && ser Associated == [2]
+     && ser Disassociated == [3]
+     && ser SyncLost == [4]
+     && ser CoordinatorRealignment == [5]
+     && ser CoordinatorStarted == [6]
+    where ser = BS.unpack . runPut . put
+
+modemStatusSerializeParse :: ModemStatus -> Bool
+modemStatusSerializeParse s = (runGet get $ runPut $ put s) == Right s
+
+instance Arbitrary ModemStatus where
+    arbitrary = elements $ enumFrom minBound
+
+
+
+--Main
+main = defaultMain tests
+
+tests :: [Test]
+tests = [
+    testGroup "FrameId" [
+        testProperty "loops around after 255" frameIdLoopsAroundAfter255,
+        testProperty "serialize and parse yield original value" frameIdSerializeParse,
+        testProperty "can parse single Word8" frameIdParseWord8
+    ],
+    testGroup "ModemStatus" [
+        testProperty "values are correctly serialized" modemStatusSerialize,
+        testProperty "serialize and then parse yields original value" modemStatusSerializeParse
+    ]]
