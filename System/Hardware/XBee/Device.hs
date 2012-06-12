088d28d331c51e8cd24ac2df2bf2c87af3c34e80
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-06-12 20:56:48 +0200

    cleanup the build

--- System/Hardware/XBee/Device.hs
+++ System/Hardware/XBee/Device.hs
@@ -7,6 +7,7 @@ module System.Hardware.XBee.Device (
     -- * Commands
     FrameCmdSpec(..),
     FramelessCmdSpec(..),
+    XBeeResult,
     resultGet,
     sendCommand,
     sendCommandAndWaitIO,
