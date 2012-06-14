680910989a1b6e85e5ba2c00a8956c942eaff37a
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-06-14 10:39:58 +0200

    add documentation

--- System/Hardware/XBee/DeviceCommand.hs
+++ System/Hardware/XBee/DeviceCommand.hs
@@ -1,8 +1,9 @@
 module System.Hardware.XBee.DeviceCommand (
+    -- * Types
+    CommandHandler,
+    CommandResponse(..),
     FrameCmdSpec(..),
     FramelessCmdSpec(..),
-    CommandHandler,
-    CommandResponse(..)
 ) where
 
 import System.Hardware.XBee.Command
@@ -10,11 +11,17 @@ import Data.SouSiT
 import Control.Monad.Identity
 
 
+-- * Answers received to a command sent to the XBee.
 data CommandResponse = CRData CommandIn
                      | CRPurged
                      | CRTimeout deriving (Show, Eq)
 
+-- * Handler for the answers to a single command sent to the XBee.
 type CommandHandler a = Sink CommandResponse Identity a
 
+-- * A command that expects an answer from the XBee.
 data FrameCmdSpec a   = FrameCmdSpec (FrameId -> CommandOut) (CommandHandler a)
+
+-- * A command that is sent without checking for an answer. Use noFrameId if the CommandOut
+--    supports frames.
 data FramelessCmdSpec = FramelessCmdSpec CommandOut
