fd48da5849553c1a57659f7a4679a0e66e04e687
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-06-14 10:49:22 +0200

    add the send command

--- System/Hardware/XBee/DeviceCommand.hs
+++ System/Hardware/XBee/DeviceCommand.hs
@@ -6,9 +6,10 @@ module System.Hardware.XBee.DeviceCommand (
     FramelessCmdSpec(..),
 ) where
 
-import System.Hardware.XBee.Command
+import Data.Word
 import Data.SouSiT
 import Control.Monad.Identity
+import System.Hardware.XBee.Command
 
 
 -- * Answers received to a command sent to the XBee.
@@ -25,3 +26,18 @@ data FrameCmdSpec a   = FrameCmdSpec (FrameId -> CommandOut) (CommandHandler a)
 -- * A command that is sent without checking for an answer. Use noFrameId if the CommandOut
 --    supports frames.
 data FramelessCmdSpec = FramelessCmdSpec CommandOut
+
+
+singleAnswer :: (CommandIn -> a) -> a -> CommandHandler a
+singleAnswer f failValue = SinkCont step (return failValue)
+    where step (CRData i) = return $ SinkDone (return $ f i)
+          step _          = return $ SinkDone (return failValue)
+
+
+-- Sends up to 100 bytes to another XBee.
+send :: XBeeAddress -> [Word8] -> FrameCmdSpec TransmitStatus
+send to d = FrameCmdSpec (cmd to) (singleAnswer handler TransmitNoAck)
+    where cmd (XBeeAddress64 to) f = Transmit64 f to False False d
+          cmd (XBeeAddress16 to) f = Transmit16 f to False False d
+          handler (TransmitResponse _ r) = r
+          handler _ = TransmitNoAck
