030d43b7e4dadf95afe24edf85456ed41193bbf3
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-06-14 13:20:52 +0200

    fix comments

--- System/Hardware/XBee/DeviceCommand.hs
+++ System/Hardware/XBee/DeviceCommand.hs
@@ -14,19 +14,19 @@ import Control.Monad.Identity
 import System.Hardware.XBee.Command
 
 
--- * Answers received to a command sent to the XBee.
+-- | Answers received to a command sent to the XBee.
 data CommandResponse = CRData CommandIn
                      | CRPurged
                      | CRTimeout deriving (Show, Eq)
 
--- * Handler for the answers to a single command sent to the XBee.
+-- | Handler for the answers to a single command sent to the XBee.
 type CommandHandler a = Sink CommandResponse Identity a
 
--- * A command that expects an answer from the XBee.
+-- | A command that expects an answer from the XBee.
 data FrameCmdSpec a   = FrameCmdSpec (FrameId -> CommandOut) (CommandHandler a)
 
--- * A command that is sent without checking for an answer. Use noFrameId if the CommandOut
---    supports frames.
+-- | A command that is sent without checking for an answer. Use noFrameId if the CommandOut
+--   supports frames.
 data FramelessCmdSpec = FramelessCmdSpec CommandOut
 
 
