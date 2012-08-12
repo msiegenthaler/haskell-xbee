434f89a020d217bb7020700ba2ca535cd035918c
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-08-12 19:30:10 +0200

    generalize some of the code in the handle-connector into the common module.

--- System/Hardware/XBee/Connector/Common.hs
+++ System/Hardware/XBee/Connector/Common.hs
@@ -1,11 +1,28 @@
 module System.Hardware.XBee.Connector.Common (
-    timeoutSink
+    -- * Connector
+    ThreadedConnector,
+    sousiConnector,
+    -- * Transformers
+    byteStringToCmdIn,
+    bytesToCmdIn,
+    cmdOutToByteString,
+    cmdOutToBytes,
+    -- * Other
+    timeoutSink,
+    debugToSysout
 ) where
 
 import Data.SouSiT
+import qualified Data.SouSiT.Trans as T
+import qualified Data.ByteString as BS
+import Data.Serialize
 import Control.Concurrent
+import Control.Concurrent.ThreadGroup
 import Control.Monad
 import System.Hardware.XBee.Device
+import System.Hardware.XBee.Command
+import System.Hardware.XBee.Frame
+
 
 
 -- | Sink that schedules the received. The sink is never done.
@@ -15,3 +32,39 @@ timeoutSink = SinkCont next done
     where next i = (return i) >>= forkIO . schedule >> return (SinkCont next done)
           done = return ()
           schedule (Scheduled time action) = threadDelay time >> action
+
+
+type ThreadedConnector = XBeeConnector ThreadGroup
+
+-- | XBeeConnector based upon a CommandIn source and a CommandOut sink.
+sousiConnector :: Source src => IO (src IO CommandIn, Sink CommandOut IO ()) -> ThreadedConnector
+sousiConnector c = XBeeConnector o close
+    where o xif = c >>= open xif
+          open xif (src,sink) = startThreadGroup [
+                                    toSchedule xif $$ timeoutSink,
+                                    src $$ incoming xif,
+                                    outgoing xif $$ sink ]
+          close = killThreadGroup
+
+-- | Parses CommandIn from ByteStrings.
+byteStringToCmdIn = (T.map BS.unpack) =$= T.disperse =$= bytesToCmdIn
+
+-- | Parses CommandIn from bytes.
+bytesToCmdIn = word8ToFrame =$= T.map frameToCommand =$= T.eitherRight
+
+-- | Serializes CommandOut into ByteString.
+cmdOutToByteString = T.map commandToFrame =$= T.map (runPut . put)
+
+-- | Serializes CommandOut into bytes.
+cmdOutToBytes = cmdOutToByteString =$= (T.map BS.unpack) =$= T.disperse
+
+
+-- | Decorates a source/sink pair to also output everything received/sent to sysout.
+--   The output is prefixed with either '>' or '<'
+debugToSysout (src,sink) = (debugSource src, debugSink sink)
+
+debugSource :: (Source src, Show a) => src IO a -> BasicSource IO a
+debugSource = decorateSource (putStrLn . ("< "++) . show)
+
+debugSink :: Show a => Sink a IO r -> Sink a IO r
+debugSink   = decorateSink (putStrLn . ("> "++) . show)
--- System/Hardware/XBee/Connector/Handle.hs
+++ System/Hardware/XBee/Connector/Handle.hs
@@ -1,52 +1,47 @@
 module System.Hardware.XBee.Connector.Handle (
-    handleConnector
+    -- * Connector
+    handleConnector,
+    handleDebugConnector,
+    -- * Low-Level
+    portSource,
+    portSink
 ) where
 
-import Prelude hiding (catch)
-import Control.Exception
-import Control.Monad
-import Control.Concurrent
-import Control.Concurrent.ThreadGroup
 import Data.SouSiT
 import Data.SouSiT.Handle
 import qualified Data.SouSiT.Trans as T
 import qualified Data.ByteString as BS
-import Data.Serialize
 import System.IO
-import System.Hardware.XBee.Device
-import System.Hardware.XBee.Command
-import System.Hardware.XBee.Frame
+import Control.Concurrent
+import Control.Monad
+import Control.Applicative
 import System.Hardware.XBee.Connector.Common
 
 
-type HandleConnector = XBeeConnector ThreadGroup
-
--- | XBee connector that uses a ReadWrite handle as the backing interface. EOFs are ignored.
-handleConnector :: IO Handle -> HandleConnector
-handleConnector h = XBeeConnector o close
-    where o xif = h >>= open xif
-
-open xif h = startThreadGroup [ toSchedule xif $$ timeoutSink,
-                                src $$ incoming xif,
-                                outgoing xif $$ sink ]
-    where src = commandSource h
-          sink = commandSink h
-
-close = killThreadGroup
-
-
-
 fetchSize = 256
-
 emptyInputDelay = 10000 -- in microseconds
 
-commandSink h = T.map commandToFrame =$= T.map (runPut . put) =$ hSink BS.hPut h
 
-commandSource h = (streamByteSource 256 h) $= (T.map BS.unpack) =$= T.disperse =$=
-        word8ToFrame =$= T.map frameToCommand =$= T.eitherRight
+-- | XBee connector that uses a ReadWrite handle as the backing interface. EOFs are ignored.
+handleConnector :: IO Handle -> ThreadedConnector
+handleConnector handle = sousiConnector (mk <$> handle)
+    where mk h = (portCommandSource h, portCommandSink h)
 
-streamByteSource fetchSize h = hSourceNoEOF rd h $= T.filter hasData
+-- | Same as handleConnector, but writes all in and out commands to the system out.
+handleDebugConnector :: IO Handle -> ThreadedConnector
+handleDebugConnector handle = sousiConnector (mk <$> handle)
+    where mk h = debugToSysout (portCommandSource h, portCommandSink h)
+
+
+portSink h = hSink BS.hPut h
+
+portCommandSink h = cmdOutToByteString =$ portSink h
+
+portSource :: Int -> Handle -> BasicSource IO BS.ByteString
+portSource fetchSize h = hSourceNoEOF rd h $= T.filter hasData
     where rd h = do r <- BS.hGetSome h fetchSize
                     unless (hasData r) $ threadDelay emptyInputDelay
                     return r
           hasData d = BS.length d > 0
+
+portCommandSource h = portSource fetchSize h $= byteStringToCmdIn
